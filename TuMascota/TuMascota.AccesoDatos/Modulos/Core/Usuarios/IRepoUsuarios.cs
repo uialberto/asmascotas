9d79a8810095c610d2b562d1ec7d5dd7736c6cc7
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2016-09-13 17:59:52 -0400

    Creacion estructura de carpetas para acceso datos y aplicacion

---
+++ TuMascota/TuMascota.AccesoDatos/Modulos/Core/Usuarios/IRepoUsuarios.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuMascota.AccesoDatos.Modulos.Core.Usuarios
+{
+    public interface IRepoUsuarios : IDisposable
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
---
+++ TuMascota/TuMascota.Aplicacion/Modulos/Core/Mascotas/Repo/RepoUsuarios.cs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuMascota.AccesoDatos;
+using TuMascota.AccesoDatos.Modulos.Core.Usuarios;
+
+namespace TuMascota.Aplicacion.Modulos.Core.Mascotas.Repo
+{
+    public class RepoUsuarios : IRepoUsuarios
+    {
+        public IUnitOfWork UnitOfWork
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
