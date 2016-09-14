68584dd056cb4b49b4f8da12afbd519e50c614c6
Luis Alberto Baigorria Rodas <uialberto@outlook.com>
2016-09-14 10:04:01 -0400

    Implementacion logica de dominio unitofwork y repositorios

--- TuMascota/TuMascota.AccesoDatos/Modulos/Core/Usuarios/IRepoUsuarios.cs
+++ TuMascota/TuMascota.AccesoDatos/Modulos/Core/Usuarios/IRepoUsuarios.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TuMascota.AccesoDatos.DataModels;
+using UIBASOFT.Base.Comunes.ResultOperation;
 
 namespace TuMascota.AccesoDatos.Modulos.Core.Usuarios
 {
     public interface IRepoUsuarios : IDisposable
     {
+        Task<Usuario> Obtener(int id);
+        Task<int> Crear(Usuario entity);
+        Task<Usuario> Editar(Usuario dto);
+        Task<int> Eliminar(int id);
         IUnitOfWork UnitOfWork { get; }
     }
 }
--- TuMascota/TuMascota.Aplicacion/UnitOfWorkBase.cs
+++ TuMascota/TuMascota.Aplicacion/UnitOfWorkBase.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TuMascota.AccesoDatos;
+using TuMascota.AccesoDatos.DataModels;
 
 namespace TuMascota.Aplicacion
 {
-    public class UnitOfWorkBase : IUnitOfWork
+    public class UnitOfWorkBase : MascotaModelContainer, IUnitOfWork
     {
+        private UnitOfWorkBase(string dbContext) : base(dbContext)
+        {
+
+        }
+        public static UnitOfWorkBase Create(string dbContext)
+        {
+            return new UnitOfWorkBase(dbContext);
+        }
     }
 }
